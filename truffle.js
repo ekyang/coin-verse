a8b3f87ad379e367d0732f53d5efb55ab9d99fc7
James Lim <email@wanseob.com>
2018-11-27 16:22:43 +0900

    Setup project structure

--- truffle.js
+++ truffle.js
@@ -1,4 +1,18 @@
 module.exports = {
+  migrations_directory: './migrations',
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-};
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 500,
+    },
+  },
+}

8527940381d05a1a36995cdee17feb03211da3dd
James Lim <email@wanseob.com>
2018-12-26 23:30:41 +0900

    Apply new design from coinus team

--- contracts/interfaces/dApp/BnusTokenFace.sol
+++ contracts/interfaces/dApp/BnusTokenFace.sol
@@ -1,21 +1,58 @@
 pragma solidity ^0.4.24;
+import "bancor-contracts/solidity/contracts/token/interfaces/IERC20Token.sol";
 
 contract BnusTokenFace {
-    // Follows ERC20
-    function name() public view returns (string);
 
-    // Follows ERC20
-    function symbol() public view returns (string);
+     // triggered when a smart token is deployed - the _token address is defined for forward compatibility, in case we want to trigger the event from a factory
+    event NewSmartToken(address _token);
+    // triggered when the total supply is increased
+    event Issuance(uint256 _amount);
+    // triggered when the total supply is decreased
+    event Destruction(uint256 _amount);
+    // triggered when someone transferred tokens to another
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    // triggered when someone allowed to transfer tokens from its account to another account
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    // Follows ERC20
-    function decimals() public view returns (uint8);
 
-    // Follows ERC20
-    function totalSupply() public view returns (uint256);
+    /**
+        @dev disables/enables transfers
+        can only be called by the contract owner
 
-    // Follows ERC20
-    function balanceOf(address _owner) public view returns (uint256);
+        @param _disable    true to disable transfers, false to enable them
+    */
+    function disableTransfers(bool _disable) public;
 
-    // Follows SmartToken
+
+    /**
+        @dev increases the token supply and sends the new tokens to an account
+        can only be called by the contract owner
+
+        @param _to         account to receive the new amount
+        @param _amount     amount to increase the supply by
+    */
+    function issue(address _to, uint256 _amount) public;
+
+    /**
+        @dev removes tokens from an account and decreases the token supply
+        can be called by the contract owner to destroy tokens from any account or by any holder to destroy tokens from his/her own account
+
+        @param _from       account to remove the amount from
+        @param _amount     amount to decrease the supply by
+    */
     function destroy(address _from, uint256 _amount) public;
+
+    // Follows ERC20 standard interfaces
+    function name() public view returns (string);
+    function symbol() public view returns (string);
+    function decimals() public view returns (uint8);
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address _owner) public view returns (uint256);
+    function allowance(address _owner, address _spender) public view returns (uint256) { _owner; _spender; }
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    // Token holder feature for upgradable feature
+    function withdrawTokens(IERC20Token _token, address _to, uint256 _amount) public;
 }
---
+++ contracts/interfaces/dApp/CnusPoolForNetworkRewardFace.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+contract CnusPoolForNewtorkRewardFace {
+    event Airdrop(address indexed _to, uint256 _amount);
+
+    /**
+      @dev withdraw cnus tokens held by the contract and sends them to an account
+      can only be called by the owner
+
+      @param _to      account to receive the new amount
+      @param _amount  amount to withdraw
+    */
+    function airdrop(address _to, uint256 _amount) public;
+
+    function getTotalBalance() public view returns (uint256);
+
+    function owner() public view returns (address) {}
+
+    function transferOwnership(address _newOwner) public;
+
+    function acceptOwnership() public;
+}
